275fcaeba04ec1c8f6124e04e7e861389cd9b001
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-15 17:26:14 +0600

    email and password get

---
+++ js/login.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-submit").addEventListener("click", function () {
+  const emailField = document.getElementById("email-field");
+  const email = emailField.value;
+
+  const passwordField = document.getElementById("password-field");
+  const password = passwordField.value;
+});
