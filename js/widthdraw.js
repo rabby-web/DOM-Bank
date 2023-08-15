f97d40e400853eb6ae762388d35ed896b406498b
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-15 18:41:51 +0600

    depost set

--- js/widthdraw.js
+++ js/widthdraw.js
@@ -1,10 +1,20 @@
 document.getElementById("deposit-btn").addEventListener("click", function () {
   const depositField = document.getElementById("deposit-field");
   const depositAmountString = depositField.value;
-  const depositAmount = parseInt(depositAmountString);
+  const depositAmount = parseFloat(depositAmountString);
   //   console.log(depositAmount);
 
-  const depositCart = document.getElementById("deposit");
-  const depositCartString = depositCart.innerText;
-  depositCart.innerText = depositAmount;
+  const depositTotalElement = document.getElementById("deposit");
+  const depositTotal = depositTotalElement.innerText;
+  const depositAmountTotal = parseFloat(depositTotal);
+
+  const currentDepositTotal = depositAmount + depositAmountTotal;
+  depositTotalElement.innerText = currentDepositTotal;
+
+  const balanceTotalElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  const total = previousBalanceTotal + depositAmountTotal;
+  balanceTotalElement.innerText = total;
 });
