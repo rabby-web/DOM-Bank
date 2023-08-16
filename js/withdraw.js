46834eaa2f0e49a3a50e551bcf6dbadd69c25daa
rabby-web <rabby.webdeveloper@gmail.com>
2023-08-16 10:52:20 +0600

    withdraw complete

--- js/withdraw.js
+++ js/withdraw.js
@@ -5,4 +5,17 @@ document.getElementById("withdraw-btn").addEventListener("click", function () {
 
   const withdrawTotalElement = document.getElementById("withdraw");
   const withdrawTotalElementString = withdrawTotalElement.innerText;
+  const previousWithdrawTotal = parseFloat(withdrawTotalElementString);
+
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  withdrawTotalElement.innerText = currentWithdrawTotal;
+
+  const balanceTotalElement = document.getElementById("balance-total");
+  const previousBalanceTotalString = balanceTotalElement.innerText;
+  const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+  const currentWithdrawBalance = previousBalanceTotal - newWithdrawAmount;
+  balanceTotalElement.innerText = currentWithdrawBalance;
+
+  withdrawField.value = "";
 });
